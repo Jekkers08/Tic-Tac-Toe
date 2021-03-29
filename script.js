c090faacb94f3baf65eab96f2fd249af6e401a8e
Jheric <jhericq08@gmail.com>
2021-03-29 18:30:17 -0400

    changed console.log to return inside playRound func

--- script.js
+++ script.js
@@ -8,42 +8,42 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   switch (true) {
     case playerSelection === "rock" && computerSelection === "rock":
-      console.log(`It's a tie!`);
+      return `It's a tie!`;
       break;
 
     case playerSelection === "rock" && computerSelection === "paper":
-      console.log(`Paper beats Rock. AI wins!`);
+      return `Paper beats Rock. AI wins!`;
       break;
 
     case playerSelection === "rock" && computerSelection === "scissors":
-      console.log(`Rock beats Scissors. You win!`);
+      return `Rock beats Scissors. You win!`;
       break;
 
     case playerSelection === "paper" && computerSelection === "paper":
-      console.log(`It's a tie!`);
+      return `It's a tie!`;
       break;
 
     case playerSelection === "paper" && computerSelection === "scissors":
-      console.log(`Scissors beats paper. AI wins!`);
+      return `Scissors beats paper. AI wins!`;
       break;
 
     case playerSelection === "paper" && computerSelection === "rock":
-      console.log(`Paper beats Rock. You win!`);
+      return `Paper beats Rock. You win!`;
       break;
 
     case playerSelection === "scissors" && computerSelection === "scissors":
-      console.log(`It's a tie!`);
+      return `It's a tie!`;
       break;
 
     case playerSelection === "scissors" && computerSelection === "rock":
-      console.log(`Rock beats Scissors. AI wins!`);
+      return `Rock beats Scissors. AI wins!`;
       break;
 
     case playerSelection === "scissors" && computerSelection === "paper":
-      console.log(`Scissors beats Paper. You win!`);
+      return `Scissors beats Paper. You win!`;
       break;
 
     default:
-      console.log(`Please enter "Rock", "Paper", or "Scissors"`);
+      return `Please enter "Rock", "Paper", or "Scissors"`;
   }
 }
