95af5a71558a7cfd003ca85f1352868a53457538
Jheric <jhericq08@gmail.com>
2021-03-30 18:17:06 -0400

    added score variables then added game and gameLoop functions

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let userScore = 0;
+let aiScore = 0;
+
 function computerPlay() {
   const options = ["rock", "paper", "scissors"];
 
@@ -9,41 +12,54 @@ function playRound(playerSelection, computerSelection) {
   switch (true) {
     case playerSelection === "rock" && computerSelection === "rock":
       return `It's a tie!`;
-      break;
 
     case playerSelection === "rock" && computerSelection === "paper":
+      aiScore++;
       return `Paper beats Rock. AI wins!`;
-      break;
 
     case playerSelection === "rock" && computerSelection === "scissors":
+      userScore++;
       return `Rock beats Scissors. You win!`;
-      break;
 
     case playerSelection === "paper" && computerSelection === "paper":
       return `It's a tie!`;
-      break;
 
     case playerSelection === "paper" && computerSelection === "scissors":
+      aiScore++;
       return `Scissors beats paper. AI wins!`;
-      break;
 
     case playerSelection === "paper" && computerSelection === "rock":
+      userScore++;
       return `Paper beats Rock. You win!`;
-      break;
 
     case playerSelection === "scissors" && computerSelection === "scissors":
       return `It's a tie!`;
-      break;
 
     case playerSelection === "scissors" && computerSelection === "rock":
+      aiScore++;
       return `Rock beats Scissors. AI wins!`;
-      break;
 
     case playerSelection === "scissors" && computerSelection === "paper":
+      userScore++;
       return `Scissors beats Paper. You win!`;
-      break;
 
     default:
       return `Please enter "Rock", "Paper", or "Scissors"`;
   }
 }
+
+function game() {
+  let playerSelect = window.prompt(`Rock, Paper, or Scissors?`).toLowerCase();
+  let computerSelect = computerPlay();
+  console.log(playRound(playerSelect, computerSelect));
+  console.log(`Your score is ${userScore}`);
+  console.log(`AI score is ${aiScore}`);
+}
+
+function gameLoop() {
+  for (i = 0; i < 5; i++) {
+    game();
+  }
+}
+
+gameLoop();
